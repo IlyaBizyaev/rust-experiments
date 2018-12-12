6eb3a0b08be1d37ef1f0b22c69a99815e970526c
Ilya Bizyaev <bizyaev@zoho.com>
2018-12-12 20:25:10 +0300

    Add z-function calculation

---
+++ algorithms/strings/z-function.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::cmp::min;
+
+fn z_function(s: &[u8]) -> Vec<usize> {
+    let n = s.len();
+    let mut z = vec![0; n];
+    let (mut l, mut r) = (0, 0);
+    for i in 1..n {
+        if i <= r {
+            z[i] = min(r - i + 1, z[i - l]);
+        }
+        while i + z[i] < n && s[z[i]] == s[i + z[i]] {
+            z[i] += 1;
+        }
+        if i + z[i] - 1 > r {
+            l = i; r = i + z[i] - 1;
+        }
+    }
+    z
+}
+
+fn main() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("Failed to read a string");
+    let n = s.trim_right().len();
+    s.truncate(n);
+    let result = z_function(s.as_bytes());
+    for x in &result[1..] {
+        print!("{} ", x);
+    }
+}
